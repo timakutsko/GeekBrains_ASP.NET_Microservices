e7b7d68e0861b4b29c63ca27b2c25fe0b6f7dc14
Tsimafei <tima.kutsko@gmail.com>
2022-04-10 20:48:26 +0300

    Lesson1

---
+++ Lesson1/FirstWebApplication/FirstWebApplication/ValuesHolder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstWebApplication
+{
+    public class ValuesHolder
+    {
+        public List<MyValue> Values { get; set; } = new List<MyValue>();
+
+        public void Add (string date, string value)
+        {
+
+            Values.Add(new MyValue
+            {
+                Date = date,
+                Value = value
+            }); 
+        }
+
+        public List<MyValue> Get()
+        {
+            return Values;
+        }
+    }
+
+    public class MyValue
+    {
+        public string Date { get; set; }
+        public string Value { get; set; }
+    }
+}
